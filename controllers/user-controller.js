2db954470cd3bda13aad67265956c015dda5f6fc
kvammee <elliott.kvamme@gmail.com>
2021-12-20 16:05:36 -0600

    Created more user functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,22 +9,44 @@ const userController = {
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
-            })
+            });
     },
 
     // Get 1 user by id
     getUser({ params }, res) {
-
+        User.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: "User not found!" });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // Create a new user
     createUser({ body }, res) {
-
+        User.create(body) 
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
     },
     
     // Update a user
     updateUser({ params, body }, res) {
-
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: "User not found!" });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
     },
     
     // Delete a user
