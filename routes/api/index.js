7029c8813ea31f33f78515bb4ba9055a33b360f9
kvammee <elliott.kvamme@gmail.com>
2021-12-20 15:21:02 -0600

    Created start of user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+router
+    .route('/users')
+    .get()      // All Users
+    .get()      // Single User
+    .post()     // New User
+    .put()      // Update User
+    .delete()   // Delete User
+
 module.exports = router;
