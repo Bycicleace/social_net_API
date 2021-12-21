e4cec68d31f5e8d46b836916ced467c1bf0e9ae4
kvammee <elliott.kvamme@gmail.com>
2021-12-21 10:54:32 -0600

    Created Thought model and basic structure

---
+++ models/Thought.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {},
+        createdAt: {},
+        username: {},
+        reactions: [],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
