1954b5fa95d03d9b99d3313bfb24abbc8a6dcc45
kvammee <elliott.kvamme@gmail.com>
2021-12-21 11:30:53 -0600

    Connected controller to api routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const { getAllThoughts,
+        getOneThought,
+        createThought,
+        updateThought,
+        removeThought } = require('../../controllers/thought-controller.js');
+
+router
+    .route('/')
+    .get(getAllThoughts)        // Get all thoughts
+    .post(createThought)        // Create new thought
+
+router
+    .route('/:id')
+    .get(getOneThought)         // Get one thought
+    .put(updateThought)         // Update one thought
+    .delete(removeThought)      // Remove one thought
 
 module.exports = router;
