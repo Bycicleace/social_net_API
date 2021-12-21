c5987e9793b3e0176bb1765c957c15d7d6b1880e
kvammee <elliott.kvamme@gmail.com>
2021-12-21 11:12:15 -0600

    Added Thought controller skeleton

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { Thought } = require('../models/index.js');
+
+const thoughtController = {
+    // GET all all thoughts
+
+    // GET single thought by id
+
+    // POST new thought
+
+    // PUT and update a current thought by id
+
+    // DELETE a thought by id
+};
+
+module.exports = thoughtController;
