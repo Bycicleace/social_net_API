1edf81b33016333cf81a27fa20f94c7bbf0e38e0
kvammee <elliott.kvamme@gmail.com>
2021-12-21 11:14:58 -0600

    Added function skeletons to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,14 +2,29 @@ const { Thought } = require('../models/index.js');
 
 const thoughtController = {
     // GET all all thoughts
+    getAllThoughts(req, res) {
+
+    },
 
     // GET single thought by id
+    getOneThought({ params }, res) {
+
+    },
 
     // POST new thought
+    createThought({ body }, res) {
+
+    },
 
     // PUT and update a current thought by id
+    updateThought({ params, body }, res) {
+
+    },
 
     // DELETE a thought by id
+    removeThought({ params }, res) {
+
+    }
 };
 
 module.exports = thoughtController;
